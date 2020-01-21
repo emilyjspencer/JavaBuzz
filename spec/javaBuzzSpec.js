c092089f4c14cd587c4cc9d9e2e39824a8cc87d2
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-01-21 11:01:14 +0000

    Returns true if the number passed to the function is divisible by 3. Returns false if not

--- spec/javaBuzzSpec.js
+++ spec/javaBuzzSpec.js
@@ -9,6 +9,16 @@
       javabuzz = new Javabuzz(); // set up - new instance
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
+
+  });
+
+  describe ('knows when a number is NOT', function() {
+
+    it('divisible by 3', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
   });
 });
 
--- src/javaBuzz.js
+++ src/javaBuzz.js
@@ -5,5 +5,6 @@ var Javabuzz = function() {};
 // define the method
 
 Javabuzz.prototype.isDivisibleByThree = function(number) {
-  return true;
+  //return true; harcoded true - will cause the next test to fail
+  return (number % 3 == 0); // will return true if divisible by 3 otherwise will return false 
 };
