368ba9b652c4e6f14671e71c3db7d5ff3bdf8e9c
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-01-21 11:22:56 +0000

    Numbers divisible by 5

--- spec/javaBuzzSpec.js
+++ spec/javaBuzzSpec.js
@@ -10,6 +10,11 @@
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
 
+    it('divisible by 5', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
   });
 
   describe ('knows when a number is NOT', function() {
@@ -17,6 +22,11 @@
     it('divisible by 3', function() {
       javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+    it('divisible by 5', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFive(2)).toBe(false);
     });
 
   });
--- src/javaBuzz.js
+++ src/javaBuzz.js
@@ -4,7 +4,15 @@ var Javabuzz = function() {};
 
 // define the method
 
-Javabuzz.prototype.isDivisibleByThree = function(number) {
+Javabuzz.prototype.isDivisibleByThree = function(number) {  // this refers to the number passed in as an arg 
+  
   //return true; harcoded true - will cause the next test to fail
-  return (number % 3 == 0); // will return true if divisible by 3 otherwise will return false 
+  return (number % 3 == 0) // will return true if divisible by 3 otherwise will return false 
+
 };
+
+Javabuzz.prototype.isDivisibleByFive = function(number) {
+
+  return (number % 5 == 0)
+};
+
